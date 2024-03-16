3e7d538ca882730d42bf4a7616a996d267f7527e
D0V4H-K11N <ledio.duda@gmail.com>
2024-03-16 21:33:21 +0100

    Finalytics

--- src/App.js
+++ src/App.js
@@ -7,27 +7,27 @@ import GridContainer from "./components/GridContainer/GridContainer";
 function App() {
   const gridItemsData = [
     {
-      imageUrl: "/Bocconi.jpg",
+      imageUrl: "Bocconi.jpg",
       text: "Currently studying in Bocconi University in Milano in the Bachelor for Economics, Management and Computer Science degree as Class of 2026",
     },
     {
-      imageUrl: "/TurgutOzal.jpg",
+      imageUrl: "TurgutOzal.jpg",
       text: "Graduated Turgut Ozal Durres with a 10.0/10.0 after attending secondary and high school there.",
     },
     {
-      imageUrl: "/Vodafone.jpg",
+      imageUrl: "Vodafone.jpg",
       text: "Successfully completed a week-long course regarding HTML and CSS in the Vodafone Albania offices in 2018. I was also offered an internship in a few AI projects which will happen this summer.",
     },
     {
-      imageUrl: "/Lego.jpg",
+      imageUrl: "Lego.jpg",
       text: "Won 1st Place after two weeks of programming a robot comprised of Motor parts, an Arduino and Legos that was able to complete challenges in record time.",
     },
     {
-      imageUrl: "/Roblox.jpg",
+      imageUrl: "Roblox.jpg",
       text: "Creator of many Roblox experiences and games, proficient in Lua used in programming, Blender for 3D Models and game development in Roblox.",
     },
     {
-      imageUrl: "/Klubi.jpg",
+      imageUrl: "Klubi.jpg",
       text: "Volunteer for a humanitarian non-profit organisation 'Klubi Shpresa' for the last 3 years.",
     },
   ];
