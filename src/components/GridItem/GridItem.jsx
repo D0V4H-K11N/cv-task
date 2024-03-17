063a2269adb88e5407ebfcafcb14a115a85d36cd
D0V4H-K11N <ledio.duda@gmail.com>
2024-03-17 15:02:09 +0100

    Parallax

--- src/components/GridItem/GridItem.jsx
+++ src/components/GridItem/GridItem.jsx
@@ -1,9 +1,29 @@
 // GridItem.jsx
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 const GridItem = ({ imageUrl, initialText, index }) => {
   const storageKey = `gridItemText_${index}`;
+  const [rotation, setRotation] = useState("rotateX(0deg) rotateY(0deg)");
+
+  const handleMouseMove = (event) => {
+    const rect = event.target.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const angleX = (rect.height / 2 - y) / 10; // Adjust the divisor for sensitivity
+    const angleY = (x - rect.width / 2) / 10; // Adjust the divisor for sensitivity
+    const popupFactor = 1.15; // The factor by which the item is scaled up on hover
+    const scale = popupFactor - 1; // The scale difference from the default size
+    const scaleFactorX = popupFactor + scale * (1 - y / rect.height);
+    const scaleFactorY = popupFactor + scale * (1 - x / rect.width);
+    setRotation(
+      `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(${scaleFactorX}, ${scaleFactorY})`
+    );
+  };
+
+  const handleMouseLeave = () => {
+    setRotation("rotateX(0deg) rotateY(0deg)");
+  };
 
   // Initialize text state with the initial text or retrieve from localStorage
   const [text, setText] = useState(() => {
@@ -27,7 +47,12 @@ const GridItem = ({ imageUrl, initialText, index }) => {
   };
 
   return (
-    <div className="grid-item">
+    <div
+      className="grid-item"
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: rotation }}
+    >
       <img src={imageUrl} alt="Image" />
       {isEditing ? (
         <input
